66d2869771df63a9b8d1224a47ed88cd0cd91e80
Corthidian <lucifer880799@gmail.com>
2023-12-18 17:33:15 +0500

    completing the other return statement also adding the round funtion in the other funtions also making the initial statement for tograyscaleluminosity

--- colorUtils.c
+++ colorUtils.c
@@ -1,5 +1,6 @@
 
 #include "colorUtils.h"
+#include <math.h>
 
 double rgbIntToFloat(int c) {
   return (c / 255.0);
@@ -17,15 +18,21 @@ int toGrayScaleAverage(int r, int g, int b) {
 }
 int min (int x, int y, int z)
 {
-  return (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
+  return round((x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z));
 }
 
 int toGrayScaleLightness (int r , int g , int b)
 {
   int max = (r > g) ? ((r > b) ? r : b) : ((g > b) ? g : b);
   int min = (r < g) ? ((r < b) ? r : b) : ((g < b) ? g : b);
-  return (max + min)/2;
+  return round((max + min)/2);
 
 }
+
+int toGrayScaleLuminosity (int r, int g, int b)
+{
+
+}
+
 
 
