744ae46eebb598383e869d20976d267da170d89a
Corthidian <lucifer880799@gmail.com>
2023-12-20 15:53:01 +0500

    making the tils program folder by running the gcc -o utilsProgram utils.c utilsTester.c -lm command in terminal

--- utilstester.c
+++ utilstester.c
@@ -7,6 +7,6 @@ int main()
     printf("%.4f\n", radianResult);
     double airDistance = getAirDistance(37.7749, -122.4194, 34.0522, -118.2437);
     printf("%.4f\n", airDistance);
-    double dilatedTime = lorentzTimeDilation(1.0, 25.0);
+    double dilatedTime = lorentzTimeDialation(1.0, 25.0);
     printf("%.4f\n", dilatedTime);
 }
