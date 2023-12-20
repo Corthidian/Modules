2b6436af154d24ad43496607151f3dffebd1e592
Corthidian <lucifer880799@gmail.com>
2023-12-20 14:28:26 +0500

    adding the get air ditance funtion and writing some of the logic

--- utils.c
+++ utils.c
@@ -7,3 +7,10 @@ double degreesToRadians (double degree)
 {
     return degree * M_PI / 180.0;
 }
+double getAirDistance(double originLatitude,double originLongitude, double destinationLatitude, double destinationLongitude)
+{
+    double deltaLongitude = degreesToRadians(destinationLongitude - originLongitude);
+    originLatitude = degreesToRadians(originLatitude);
+    destinationLatitude = degreesToRadians(destinationLatitude);
+}
+
