b6378d5f42028c84e8e8af6af5201076be3d96e3
Corthidian <lucifer880799@gmail.com>
2023-12-20 15:44:30 +0500

    completing the utilstest file

--- utilstester.c
+++ utilstester.c
@@ -4,5 +4,9 @@
 int main()
 {
     double radianResult = degreesToRadians(90.0);
-    
+    printf("%.4f\n", radianResult);
+    double airDistance = getAirDistance(37.7749, -122.4194, 34.0522, -118.2437);
+    printf("%.4f\n", airDistance);
+    double dilatedTime = lorentzTimeDilation(1.0, 25.0);
+    printf("%.4f\n", dilatedTime);
 }
