352cad34b6bce6d26dfbf92acc1a37d20d44166a
Corthidian <lucifer880799@gmail.com>
2023-12-20 14:21:59 +0500

    making the header file for hack five called utils.h as well declaring the prototypes

---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+double degreesToRadians(double degree);
+double getAirDistance(double originLatitude,double originLongitude, double destinationLatitude, double destinationLogitude);
+double lorentzTimeDialation (double time, double percentC);
+
+#endif
+
